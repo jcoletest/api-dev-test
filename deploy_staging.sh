fd528c4942268afbafbf8ee10ee7f513261f870f
J Cole Morrison <j.cole.morrison@gmail.com>
2016-12-13 20:36:32 -0800

    add awslogs

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -38,7 +38,10 @@ function update_api_service () {
     | $JQ ".containerDefinitions[0].environment |= .+ [{\"name\": \"STAGING_RDS_DB\", \"value\": \"$STAGING_RDS_DB\"}]" \
     | $JQ ".containerDefinitions[0].environment |= .+ [{\"name\": \"STAGING_RDS_USER\", \"value\": \"$STAGING_RDS_USER\"}]" \
     | $JQ ".containerDefinitions[0].environment |= .+ [{\"name\": \"STAGING_RDS_PWD\", \"value\": \"$STAGING_RDS_PWD\"}]" \
-    | $JQ ".containerDefinitions[0].environment |= .+ [{\"name\": \"NODE_ENV\", \"value\": \"staging\"}]" > ./updated-task.json
+    | $JQ ".containerDefinitions[0].environment |= .+ [{\"name\": \"NODE_ENV\", \"value\": \"staging\"}]" \
+    | $JQ ".containerDefinitions[0].logConfiguration.options[\"awslogs-group\"] = \"$STAGING_API_AWSLOGS_GROUP\"" \
+    | $JQ ".containerDefinitions[0].logConfiguration.options[\"awslogs-region\"] = \"$STAGING_API_AWSLOGS_REGION\"" \
+    | $JQ ".containerDefinitions[0].logConfiguration.options[\"awslogs-stream-prefix\"] = \"$STAGING_API_AWSLOGS_PREFIX\"" > ./updated-task.json
 
   aws ecs register-task-definition \
     --cli-input-json file://updated-task.json
@@ -50,75 +53,9 @@ function update_api_service () {
   rm -rf ./updated-task.json
 }
 
-function all_but_migrate () {
+function update_ecs () {
   push_to_registry
   update_api_service
 }
 
-function help_menu () {
-cat << EOF
-Usage: ${0} (-h | -p | -i | -r | -d | -a)
-
-OPTIONS:
-   -h|--help             Show this message
-   -p|--push-to-registry Push the api application to your private registry
-   -w|--update-api       Update the api application
-   -r|--update-worker    Update the background worker
-   -d|--run-db-migrate   Run a database migration
-   -a|--all-but-migrate  Do everything except migrate the database
-
-EXAMPLES:
-   Push the api application to your private registry:
-        $ ./deploy.sh -p
-
-   Update the api application:
-        $ ./deploy.sh -i
-
-   Update the background worker:
-        $ ./deploy.sh -r
-
-   Run a database migration:
-        $ ./deploy.sh -d
-
-   Do everything except run a database migration:
-        $ ./deploy.sh -a
-
-EOF
-}
-
-all_but_migrate
-
-# Deal with command line flags.
-# while [[ $# > 0 ]]
-# do
-# case "${1}" in
-#   -p|--push-to-registry)
-#   push_to_registry
-#   shift
-#   ;;
-#   -i|--update-api)
-#   update_api_service
-#   shift
-#   ;;
-#   -r|--update-worker)
-#   update_worker_service
-#   shift
-#   ;;
-#   -d|--run-db-migrate)
-#   run_database_migration
-#   shift
-#   ;;
-#   -a|--all-but-migrate)
-#   all_but_migrate
-#   shift
-#   ;;
-#   -h|--help)
-#   help_menu
-#   shift
-#   ;;
-#   *)
-#   echo "${1} is not a valid flag, try running: ${0} --help"
-#   ;;
-# esac
-# shift
-# done
+update_ecs
