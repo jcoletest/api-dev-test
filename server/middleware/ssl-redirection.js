bdf90e41d508bcb5a17b221b98f1bf18783df9dc
J Cole Morrison <j.cole.morrison@gmail.com>
2016-12-21 20:51:24 -0800

    use correct env vars

--- server/middleware/ssl-redirection.js
+++ server/middleware/ssl-redirection.js
@@ -4,7 +4,7 @@ const join = require('path').join
 module.exports = function (options) {
   return function sslRedirection (req, res, next) {
     const env = process.env.NODE_ENV
-    if (env === 'STAGING' || 'PRODUCTION') {
+    if (env === 'staging' || 'production') {
       if (req.headers['x-forwarded-proto'] === 'https') return next()
       return res.redirect(301, `https://${join(req.hostname, req.url)}`)
     }
