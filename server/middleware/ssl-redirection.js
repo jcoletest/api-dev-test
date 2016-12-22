851d19a0ac311d51a532441cea513a0ede273385
J Cole Morrison <j.cole.morrison@gmail.com>
2016-12-21 20:37:06 -0800

    actually return the middleware

--- server/middleware/ssl-redirection.js
+++ server/middleware/ssl-redirection.js
@@ -2,7 +2,7 @@
 const join = require('path').join
 
 module.exports = function (options) {
-  function sslRedirection (req, res, next) {
+  return function sslRedirection (req, res, next) {
     const env = process.env.NODE_ENV
     if (env === 'STAGING' || 'PRODUCTION') {
       if (req.headers['x-forwarded-proto'] === 'https') return next()
